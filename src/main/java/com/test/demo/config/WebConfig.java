5ff3e37a874d21053898e8de9694f2207ec7673a
Thanh279 <drawin278@gmail.com>
2025-04-10 23:29:00 +0700

    Chỉnh sửa WebConfig và file cấu hình Maven

--- src/main/java/com/test/demo/config/WebConfig.java
+++ src/main/java/com/test/demo/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("http://localhost:5173") 
+                // .allowedOrigins("http://localhost:5173") 
+                .allowedOrigins("https://hh3d.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
                 .allowedHeaders("*") 
                 .allowCredentials(true); 
